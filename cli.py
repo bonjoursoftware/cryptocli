e37743d9618f5fc71dd11176c4bbbe2e2bd1fc23
Antoine <achatain@users.noreply.github.com>
2021-03-18 23:58:57 +0000

    Expand cli with find symbols

--- cli.py
+++ cli.py
@@ -19,35 +19,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see
 # https://github.com/bonjoursoftware/cryptocli/blob/master/LICENSE
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from threading import Event
-from typing import Any, Tuple
 
 from cryptocli.cryptocurrency import Cryptocurrency
 from cryptocli.exceptions import CryptoCLIException
 
 
-def parse_args() -> Tuple[Any, Any]:
+def parse_args() -> None:
     parser = ArgumentParser(description="fetches live cryptocurrency prices")
-    parser.add_argument("-s", "--symbol", type=str, help="cryptocurrency symbol", default="BTC-GBP")
-    parser.add_argument("-t", "--ticker", type=int, help="ticker interval in seconds", default=300)
+    parser.set_defaults(func=lambda _: print(parser.format_help()))
+
+    subparsers = parser.add_subparsers()
+
+    price_parser = subparsers.add_parser("price")
+    price_parser.add_argument("-s", "--symbol", type=str, help="cryptocurrency symbol", default="BTC-GBP")
+    price_parser.add_argument("-t", "--ticker", type=int, help="ticker interval in seconds", default=300)
+    price_parser.set_defaults(func=watch_last_trade_price)
+
+    find_parser = subparsers.add_parser("find")
+    find_parser.add_argument("-s", "--symbol", type=str, required=True, help="cryptocurrency symbol")
+    find_parser.set_defaults(func=find_symbols)
+
     args = parser.parse_args()
-    return args.symbol, args.ticker
+    args.func(args)
 
 
-def print_last_trade_price(symbol: str) -> None:
+def watch_last_trade_price(args: Namespace):
+    last_trade_price(args.symbol)
+    crypto_ticker = Event()
+    while not crypto_ticker.wait(args.ticker):
+        last_trade_price(args.symbol)
+
+
+def last_trade_price(symbol: str) -> None:
     try:
         print(Cryptocurrency().last_trade_price(symbol))
     except CryptoCLIException as ex:
         print(ex)
 
 
+def find_symbols(args: Namespace) -> None:
+    try:
+        print(Cryptocurrency().find_symbols(args.symbol))
+    except CryptoCLIException as ex:
+        print(ex)
+
+
 def main() -> None:
-    symbol, ticker = parse_args()
-    print_last_trade_price(symbol)
-    crypto_ticker = Event()
-    while not crypto_ticker.wait(ticker):
-        print_last_trade_price(symbol)
+    parse_args()
 
 
 if __name__ == "__main__":
